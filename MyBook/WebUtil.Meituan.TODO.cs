e9adf1034e6b306ca3f2d264ff12d1e6b71d34cd
xyzkljl1 <xyzkljl1@yahoo.com>
2026-05-28 01:25:17 +0800

    AI generated (OpenAI GPT-5 Codex): Mark TODO utility modules

---
+++ MyBook/WebUtil.Meituan.TODO.cs
@@ -0,0 +1,11 @@
+namespace MyBook
+{
+    // TODO: Fetch Meituan balance from the web page.
+    partial class WebUtil
+    {
+        public Task<Currency> FetchMeituanBalance()
+        {
+            throw new NotImplementedException("TODO: Fetch Meituan balance from the web page.");
+        }
+    }
+}
